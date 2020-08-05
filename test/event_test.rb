915c1bd7a37360711018337e49e53d0e96f87b94
RyanDBarnett <conner2468@gmail.com>
2020-08-05 09:30:57 -0600

    Create event exists test

---
+++ test/event_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/food_truck'
+require './lib/item'
+require './lib/event'
+
+class EventTest < Minitest::Test
+  def setup
+    @event1 = Event.new('South Pearl Street Farmers Market')
+  end
+
+  def test_it_exists
+    assert_instance_of Event, @event1
+  end
+end
