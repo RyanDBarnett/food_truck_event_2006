77c2ececc43b786a4905ec1e5c797a57ea13a65b
RyanDBarnett <conner2468@gmail.com>
2020-08-05 09:46:41 -0600

    Add event food trucks that sell method

--- lib/event.rb
+++ lib/event.rb
@@ -12,4 +12,10 @@ class Event
   def food_truck_names
     @food_trucks.map(&:name)
   end
+
+  def food_trucks_that_sell item
+    @food_trucks.select do |truck|
+      truck.check_stock(item) > 0
+    end
+  end
 end
