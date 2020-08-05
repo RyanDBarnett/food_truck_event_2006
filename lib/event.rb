70da9dd01bdde8bb8669d88f56010ae770915f9d
RyanDBarnett <conner2468@gmail.com>
2020-08-05 10:09:30 -0600

    Add event total inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -25,4 +25,15 @@ class Event
       list.concat(item_names)
     end.sort.uniq
   end
+
+  def total_inventory
+    @food_trucks.reduce({}) do |inventory, truck|
+      truck.inventory.keys.each do |item|
+        inventory[item] = {quantity: 0, food_trucks: []} if !inventory[item]
+        inventory[item][:food_trucks] << truck if !inventory[item][:food_trucks].include?(truck)
+        inventory[item][:quantity] += truck.inventory[item]
+      end
+      inventory
+    end
+  end
 end
